c0976f54e6ded12217493e58039950ad090aad68
Vonckad <Vonkad@yandex.ru>
2021-02-22 15:13:52 +0300

    Initial Commit

---
+++ HomeWork_2_4_5_b_CleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_2_4_5_b_CleanSwift
+//
+//  Created by Vlad Ralovich on 2/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
